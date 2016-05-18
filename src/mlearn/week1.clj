fbbdbb755309794fffafa01ceb101d8715e0dc3a
Eric Lavigne <lavigne.eric@gmail.com>
2016-05-18 00:22:57 -0400

    Cost function with vectorization (2ms to create, 0.1ms to use)

--- src/mlearn/week1.clj
+++ src/mlearn/week1.clj
@@ -1,12 +1,37 @@
 (ns mlearn.week1
-  (:require [clojure.math.numeric-tower :as math]))
+  (:use [uncomplicate.neanderthal core math native]))
 
-(defn sum [list-of-numbers]
-  (reduce + 0.0 list-of-numbers))
+(defn powers
+  "(powers x n) -> [x^0 x^1 x^2 ... x^(n-1)]"
+  [x n]
+  (map
+    #(pow x %)
+    (range n)))
 
 (defn evaluate-polynomial
   "h(x) = theta0 + theta1 * x + theta2 * x^2 * ..."
   [x thetas]
-  (let [x-powers (map #(math/expt x %)
-                   (range (count thetas)))]
-    (sum (map * thetas x-powers))))
+  (let [x-powers (powers x (count thetas))]
+    (apply + (map * thetas x-powers))))
+
+(defn matrix-sum [matrix]
+  (apply + (flatten (seq matrix))))
+
+(defn create-cost-function
+  "Returns cost function J(thetas) = average((h(x) - y)^2)/2"
+  [xy-pairs count-thetas]
+  (let [theta-exponents (range count-thetas)
+        xs (map :x xy-pairs)
+        ys (map :y xy-pairs)
+        sample-size (count xy-pairs)
+        y-matrix (dge 1 sample-size ys)
+        x-powers (mapcat #(powers % count-thetas) xs)
+        x-matrix (dge count-thetas sample-size x-powers)
+        one-over-2m (/ 0.5 sample-size)]
+    (fn [thetas]
+      (let [h (mm
+                (dge 1 count-thetas thetas)
+                x-matrix)
+            diff (axpy -1 h y-matrix)
+            diff-squared (matrix-sum (mm diff (trans diff)))]
+        (* one-over-2m diff-squared)))))
--- test/mlearn/week1_test.clj
+++ test/mlearn/week1_test.clj
@@ -7,3 +7,15 @@
     (is (= 1.0 (evaluate-polynomial 0.0 [1.0 0.5])))
     (is (= 2.0 (evaluate-polynomial 2.0 [1.0 0.5])))
     (is (= 2.5 (evaluate-polynomial 3.0 [1.0 0.5])))))
+
+(deftest cost-function-tests
+  (testing "Cost function for perfectly linear training set"
+    (let [data [{:x 0 :y 1} {:x 1 :y 1.5} {:x 2 :y 2} {:x 3 :y 2.5}]
+          cost (create-cost-function data 2)
+          correct-model [1 0.5]
+          close-model [0.8 0.6]
+          far-model [3 -2]]
+      (is (= 0.0 (cost correct-model)))
+      (is (< 0
+            (cost close-model)
+            (cost far-model))))))
